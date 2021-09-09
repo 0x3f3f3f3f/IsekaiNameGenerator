fdc0668cc9771a775a693009c842c70b04e1311f
云却 <479813005@qq.com>
2021-09-09 14:54:42 +0800

    初始化

---
+++ IsekaiNameGenerator/Program.cs
@@ -0,0 +1,85 @@
+﻿using Megumin.Isekai.Name;
+
+using System;
+using System.IO;
+using System.Text.Json;
+using System.Xml.Linq;
+
+namespace IsekaiNameGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = LoadDictNames();
+
+            Generator.InitDict(list);
+
+
+
+            while (true)
+            {
+                var k = Console.ReadKey();
+
+                if (k.Key == ConsoleKey.Enter)
+                {
+                    var name = Generator.Create(4);
+                    Console.WriteLine(name);
+                }
+                else if (k.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+            }
+        }
+
+        private static List<string> LoadDictNames()
+        {
+            var files = Directory.GetFiles(Directory.GetCurrentDirectory() + "/Dict");
+            HashSet<string> nameList = new HashSet<string>();
+            foreach (var file in files)
+            {
+                if (file.EndsWith(".txt"))
+                {
+                    var names = File.ReadAllLines(file);
+                    foreach (var item in names)
+                    {
+                        if (!string.IsNullOrEmpty(item))
+                        {
+                            nameList.Add(item);
+                        }
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        var f = File.ReadAllText(file);
+                        var json = JsonDocument.Parse(f);
+                        foreach (var name in json.RootElement.EnumerateArray())
+                        {
+                            try
+                            {
+                                nameList.Add(name.GetString());
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(name + "\n" + e);
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(file + "\n" + e);
+                    }
+                }
+
+            }
+
+            return nameList.ToList();
+        }
+    }
+}
+
+
+
---
+++ Megumin.IsekaiNameGenerator/Generator.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+
+namespace Megumin.Isekai.Name
+{
+    internal static class IEnumerableExtension
+    {
+        static Random random = new Random(DateTimeOffset.UtcNow.GetHashCode());
+        public static T Random<T>(this ICollection<T> enumerable)
+        {
+            if (enumerable.Count > 0)
+            {
+                return enumerable.ElementAt(random.Next(0, enumerable.Count));
+            }
+            return default;
+        }
+
+        public static T Random<T>(this IEnumerable<T> enumerable)
+        {
+            var count = enumerable.Count();
+            if (count > 0)
+            {
+                return enumerable.ElementAt(random.Next(0, count));
+            }
+            return default;
+        }
+    }
+
+
+    public class Generator
+    {
+        static Dictionary<int, Generator> generators = new Dictionary<int, Generator>();
+        static Dictionary<int, List<string>> toneCharactor = new Dictionary<int, List<string>>()
+        {
+            { 1,new List<string>()},
+            { 2,new List<string>()},
+            { 3,new List<string>()},
+            { 4,new List<string>()},
+            { 5,new List<string>()},
+        };
+
+        /// <summary>
+        /// 声调模式
+        /// </summary>
+        List<string> ToneMode = new List<string>();
+
+        /// <summary>
+        /// 根据字典初始化生成器
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="nameDict"></param>
+        public static void InitDict<T>(T nameDict)
+            where T : IEnumerable<string>
+        {
+
+
+            foreach (var name in nameDict)
+            {
+                if (name.Length > 0 && name.Length < 10)
+                {
+                    if (generators.TryGetValue(name.Length, out var generator))
+                    {
+
+                    }
+                    else
+                    {
+                        generator = new Generator(name.Length);
+                        generators.Add(name.Length, generator);
+                    }
+
+                    generator.InitName(name);
+                }
+            }
+        }
+
+        public static string Create(int nameLength)
+        {
+            if (generators.TryGetValue(nameLength, out var generator))
+            {
+                return generator.Create();
+            }
+
+            return "No Generator";
+        }
+
+
+
+        Random random = new Random(DateTimeOffset.UtcNow.GetHashCode());
+        internal protected Generator(int length)
+        {
+            Length = length;
+        }
+
+        public int Length { get; }
+
+
+
+        public string Create()
+        {
+            if (ToneMode.Count > 0)
+            {
+                return "No Tone Mode";
+            }
+            var mode = ToneMode.Random();
+
+            var result = "";
+            foreach (var item in mode)
+            {
+                int tone = int.Parse(item.ToString());
+                var cdict = toneCharactor[tone];
+                result += cdict.Random();
+            }
+
+            return result;
+        }
+
+        private void InitName(string name)
+        {
+            if (name.Length != Length)
+            {
+                throw new ArgumentException();
+            }
+
+        }
+    }
+}
+
+
+
